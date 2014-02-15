4d04ac43f55823346acca93bb1124c3910fa14fd
aidankmcl <aidan.mclaughlin@students.olin.edu>
2014-02-15 08:39:38 -0500

    Spring Cleaning, comments

--- app/src/main/java/com/jc/app/MakeTask.java
+++ app/src/main/java/com/jc/app/MakeTask.java
@@ -27,62 +27,72 @@ public class MakeTask extends Activity {
         setContentView(R.layout.new_task);
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //Initialize buttons
         final ImageButton addToSQL = (ImageButton) findViewById(R.id.addTask);
         final SeekBar whenIsIt = (SeekBar) findViewById(R.id.whenIsIt);
         final ImageView priority = (ImageView) findViewById(R.id.imageView);
         TextView TitleTaskMake = (TextView) findViewById(R.id.newTaskTitle);
 
+        //Setting font
         Typeface tf = Typeface.createFromAsset(getAssets(), "missiongl.otf");
         TitleTaskMake.setTypeface(tf);
 
-
-
+        //Priority image button had issues initially, kept this
+        //piece of debugging to be safe.
         priority.setVisibility(View.VISIBLE);
 
+        //Initializing Database
         db = new DBHandler(this);
         db.open();
 
+        //Setting up 'CREATE' button at bottom of screen
         addToSQL.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 EditText inputTaskName = (EditText) findViewById(R.id.inputTaskName);
                 ArrayList<Task> tasks = db.getTasks();
-                String skip = "false";
-
+                
+                //Preparing data for DB
+                String skip = "false"; //We set skip to 'true' on tasks that we don't want in DB
                 String name = inputTaskName.getText().toString();
                 Integer when = whenIsIt.getProgress();
 
+                //Checks to see if you're about to use a name that's already taken
                 for (int x = 0; x<tasks.size(); x++) {
                     if (name.equals(tasks.get(x).name)) {
                         Toast.makeText(getApplicationContext(), "Please use unique names", Toast.LENGTH_SHORT).show();
-                        skip = "true";
+                        skip = "true";  //We don't want repeat names
                         break;
                     }
                 }
 
+                //
                 if (name.equals("")) {
                     Toast.makeText(getApplicationContext(), "Choose a name!", Toast.LENGTH_SHORT).show();
-                    skip = "true";
+                    skip = "true";  // Doesn't enter empty named task
                 }
 
+                //Adding to DB
                 if (skip.equals("false")) {
-                    Task task = new Task(name,"false","none",0,when,100000,0,0);
+                    // The 100000 number is so that 'Best Time' starts out huge and goes down
+                    Task task = new Task(name,"false","none",0,when,100000,0,0); 
                     task.setId("");
                     db.addTask(task);
 
+                    //Goes back to main screen via MainActivity
                     Intent i = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(i);
                 }
 
                 if (skip.equals("true")) {
+                    //Empties input text field if no task is added
                     inputTaskName.setText("");
-//                    Intent i = new Intent(getApplicationContext(), MakeTask.class);
-//                    startActivity(i);
                 }
             }
         });
     }
 
+    //Setting upwards navigation on actionbar
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
